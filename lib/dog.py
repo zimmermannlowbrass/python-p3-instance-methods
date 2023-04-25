a31df362489627c440e1fa66e601d4f0e8baf37d
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-25 16:45:53 -0500

    completed labwork

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,10 @@
 
 class Dog:
     # Class body goes here
-
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
     #Instance method definition
-    pass
+    
